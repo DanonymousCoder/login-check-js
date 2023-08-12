bc853d88c6540743e62af7cc8b39d66118b8e584
Ridwan <aladebaruwa@gmail.com>
2023-08-12 17:39:18 +0100

    add

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Login checker
 let userName = prompt('Who are you?');
 if (input = 'Admin') {
-    let pass = prompt('Password', ' ')
+    let pass = prompt('Password', ' ');
+    if (pass = 'TheMaster') {
+        alert('Welcome')
+    }
 }
